9e438e69f6952d4f6e013c37c4454900949c6a30
Vladimir N. Silyaev <TheCBaH@gmail.com>
2017-12-19 22:43:38 -0800

    fixup! Initial version

--- lib/GdataSitesModel.ml
+++ lib/GdataSitesModel.ml
@@ -1,15 +1,17 @@
 let ns_sites = "http://schemas.google.com/sites/2008"
+
 let endpoint = "https://sites.google.com/feeds/"
 
 let get_sites_prefix namespace =
- if namespace = ns_sites then "site" else GdataACL.get_acl_prefix namespace
+  if namespace = ns_sites then "site" else GdataACL.get_acl_prefix namespace
 
 
 let parse_xml_data_model tree =
   match tree with
   | GapiCore.AnnotatedTree.Leaf
-    ([`Attribute; `Name ns ; `Namespace "http://www.w3.org/2000/xmlns/"],_) ->
-     `Namespace
+      ( [`Attribute; (`Name ns); (`Namespace "http://www.w3.org/2000/xmlns/")]
+      , _ ) ->
+      `Namespace
   | GapiCore.AnnotatedTree.Leaf
       ([`Attribute; (`Name "etag"); (`Namespace ns)], v)
     when ns = GdataAtom.ns_gd ->
@@ -77,148 +79,6 @@ let parse_xml_data_model tree =
   | e -> `Other e
 
 
-module Content = struct
-  module Entry = struct
-    type content =
-      | Empty
-      | Attachment of string * string
-      | Xhtml of GdataCore.xml_data_model
-
-    type t =
-      { etag: string
-      ; id: GdataAtom.atom_id
-      ; updated: GdataAtom.atom_updated
-      ; published: GdataAtom.atom_published
-      ; edited: GdataAtom.app_edited
-      ; category: GdataAtom.Category.t
-      ; title: GdataAtom.Title.t
-      ; links: GdataAtom.Link.t list
-      ; author: GdataAtom.Author.t
-      ; summary: GdataAtom.Summary.t
-      ; pageName: string
-      ; revision: int
-      ; extensions: GdataAtom.GenericExtensions.t
-      ; content: content }
-
-    let empty =
-      { etag= ""
-      ; id= ""
-      ; updated= GapiDate.epoch
-      ; published= GapiDate.epoch
-      ; edited= GapiDate.epoch
-      ; category= GdataAtom.Category.empty
-      ; title= GdataAtom.Title.empty
-      ; summary= GdataAtom.Summary.empty
-      ; links= []
-      ; author= GdataAtom.Author.empty
-      ; pageName= ""
-      ; revision= 0
-      ; extensions=GdataAtom.GenericExtensions.empty
-      ; content= Empty }
-
-
-    let to_xml_data_model entry =
-      GdataAtom.render_element GdataAtom.ns_atom "entry"
-        [ GdataAtom.render_attribute GdataAtom.ns_gd "etag" entry.etag
-        ; GdataAtom.render_text_element GdataAtom.ns_atom "id" entry.id
-        ; GdataAtom.render_date_element GdataAtom.ns_atom "updated"
-            entry.updated
-        ; GdataAtom.render_date_element GdataAtom.ns_app "published"
-            entry.published
-        ; GdataAtom.render_date_element GdataAtom.ns_app "edited" entry.edited
-        ; GdataAtom.Category.to_xml_data_model entry.category
-        ; GdataAtom.Title.to_xml_data_model entry.title
-        ; GdataAtom.Summary.to_xml_data_model entry.summary
-        ; GdataAtom.render_element_list GdataAtom.Link.to_xml_data_model
-            entry.links
-        ; GdataAtom.Author.to_xml_data_model entry.author
-        ; GdataAtom.render_text_element ns_sites "pageName" entry.pageName
-        ; GdataAtom.render_int_element ns_sites "revision" entry.revision
-        ; GdataAtom.GenericExtensions.to_xml_data_model entry.extensions ]
-
-
-    let of_xml_data_model entry tree =
-      match parse_xml_data_model tree with
-      | `Etag etag -> {entry with etag}
-      | `Id id -> {entry with id}
-      | `Updated updated -> {entry with updated}
-      | `Edited edited -> {entry with edited}
-      | `Category category -> {entry with category}
-      | `Published published -> {entry with published}
-      | `Title title -> {entry with title}
-      | `Link link -> {entry with links= link :: entry.links}
-      | `Author author -> {entry with author}
-      | `Summary summary -> {entry with summary}
-      | `Namespace -> entry
-      | `Other
-          GapiCore.AnnotatedTree.Node
-            ( [`Element; (`Name "pageName"); (`Namespace ns)]
-            , [(GapiCore.AnnotatedTree.Leaf ([`Text], v))] )
-        when ns = ns_sites ->
-          {entry with pageName= v}
-      | `Other
-          GapiCore.AnnotatedTree.Node
-            ( [`Element; (`Name "revision"); (`Namespace ns)]
-            , [(GapiCore.AnnotatedTree.Leaf ([`Text], v))] )
-        when ns = ns_sites ->
-          {entry with revision= int_of_string v}
-      | `Other
-          GapiCore.AnnotatedTree.Node
-            ( [`Element; (`Name "content"); (`Namespace ns)]
-            , [ (GapiCore.AnnotatedTree.Leaf
-                  ([`Attribute; (`Name "type"); (`Namespace "")], "xhtml"))
-              ; (GapiCore.AnnotatedTree.Node _ as cs) ] )
-        when ns = GdataAtom.ns_atom ->
-          {entry with content= Xhtml cs}
-      | `Other
-          GapiCore.AnnotatedTree.Node
-            ( [`Element; (`Name "content"); (`Namespace ns)]
-            , [ (GapiCore.AnnotatedTree.Leaf
-                  ([`Attribute; (`Name "type"); (`Namespace "")], content_type))
-              ; (GapiCore.AnnotatedTree.Leaf
-                  ([`Attribute; (`Name "src"); (`Namespace "")], src)) ] )
-        when ns = GdataAtom.ns_atom ->
-          {entry with content= Attachment (content_type, src)}
-      | _ ->
-          let extensions =
-              GdataAtom.GenericExtensions.of_xml_data_model
-                entry.extensions
-                tree
-            in
-              { entry with extensions }
-
-  end
-
-  module Feed =
-    GdataAtom.MakeFeed (Entry) (GdataAtom.Link) (GdataAtom.GenericExtensions)
-
-  let parse_entry =
-    GdataAtom.data_model_to_entry Entry.of_xml_data_model Entry.empty
-
-
-  let parse_feed = Feed.parse_feed
-
-  let entry_to_data_model =
-    GdataAtom.element_to_data_model get_sites_prefix Entry.to_xml_data_model
-
-
-  let feed_to_data_model =
-    GdataAtom.element_to_data_model get_sites_prefix Feed.to_xml_data_model
-
-
-  let make_category name =
-    let category_scheme = "http://schemas.google.com/g/2005#kind" in
-    { GdataAtom.Category.empty with
-      GdataAtom.Category.scheme= category_scheme
-    ; term= "http://schemas.google.com/sites/2008#" ^ name
-    ; label= name }
-
-
-  let attachment_category = make_category "attachment"
-
-  let webpage_category = make_category "webpage"
-end
-
 module Site = struct
   module Entry = struct
     type t =
@@ -243,7 +103,7 @@ module Site = struct
       ; links= []
       ; siteName= ""
       ; theme= ""
-      ; extensions= GdataAtom.GenericExtensions.empty}
+      ; extensions= GdataAtom.GenericExtensions.empty }
 
 
     let to_xml_data_model entry =
@@ -285,10 +145,9 @@ module Site = struct
         when ns = ns_sites ->
           {entry with theme= v}
       | _ ->
-         let extensions =
-           GdataAtom.GenericExtensions.of_xml_data_model
-                entry.extensions
-                tree in
+          let extensions =
+            GdataAtom.GenericExtensions.of_xml_data_model entry.extensions tree
+          in
           {entry with extensions}
 
   end
@@ -308,5 +167,194 @@ module Site = struct
 
   let feed_to_data_model =
     GdataAtom.element_to_data_model get_sites_prefix Feed.to_xml_data_model
+
+end
+
+module Content = struct
+  module Entry = struct
+    type content = Empty | Attachment of string * string | Xhtml of string
+
+    type t =
+      { etag: string
+      ; id: GdataAtom.atom_id
+      ; updated: GdataAtom.atom_updated
+      ; published: GdataAtom.atom_published
+      ; edited: GdataAtom.app_edited
+      ; category: GdataAtom.Category.t
+      ; title: GdataAtom.Title.t
+      ; links: GdataAtom.Link.t list
+      ; author: GdataAtom.Author.t
+      ; summary: GdataAtom.Summary.t
+      ; pageName: string
+      ; revision: int
+      ; extensions: GdataAtom.GenericExtensions.t
+      ; content: content }
+
+    let empty =
+      { etag= ""
+      ; id= ""
+      ; updated= GapiDate.epoch
+      ; published= GapiDate.epoch
+      ; edited= GapiDate.epoch
+      ; category= GdataAtom.Category.empty
+      ; title= GdataAtom.Title.empty
+      ; summary= GdataAtom.Summary.empty
+      ; links= []
+      ; author= GdataAtom.Author.empty
+      ; pageName= ""
+      ; revision= 0
+      ; extensions= GdataAtom.GenericExtensions.empty
+      ; content= Empty }
+
+
+    let content_to_xml_data_model = function
+      | Empty | Attachment _ -> []
+      | Xhtml html ->
+          let string_input str =
+            let off = ref 0 in
+            fun () ->
+              if !off < String.length str then
+                let ch = str.[!off] in
+                incr off ; Char.code ch
+              else raise End_of_file
+          in
+          let xhtml = GdataUtils.parse_xml (string_input html) (fun x -> x) in
+          [ GapiCore.AnnotatedTree.Node
+              ( [`Element; `Name "content"; `Namespace GdataAtom.ns_atom]
+              , [ GapiCore.AnnotatedTree.Leaf
+                    ([`Attribute; `Name "type"; `Namespace ""], "xhtml")
+                ; xhtml ] ) ]
+
+
+    let to_xml_data_model entry =
+      GdataAtom.render_element GdataAtom.ns_atom "entry"
+        [ GdataAtom.render_attribute GdataAtom.ns_gd "etag" entry.etag
+        ; GdataAtom.render_text_element GdataAtom.ns_atom "id" entry.id
+        ; GdataAtom.render_date_element GdataAtom.ns_atom "updated"
+            entry.updated
+        ; GdataAtom.render_date_element GdataAtom.ns_app "published"
+            entry.published
+        ; GdataAtom.render_date_element GdataAtom.ns_app "edited" entry.edited
+        ; GdataAtom.Category.to_xml_data_model entry.category
+        ; GdataAtom.Title.to_xml_data_model entry.title
+        ; GdataAtom.Summary.to_xml_data_model entry.summary
+        ; GdataAtom.render_element_list GdataAtom.Link.to_xml_data_model
+            entry.links
+        ; GdataAtom.Author.to_xml_data_model entry.author
+        ; GdataAtom.render_text_element ns_sites "pageName" entry.pageName
+        ; GdataAtom.render_int_element ns_sites "revision" entry.revision
+        ; content_to_xml_data_model entry.content
+        ; GdataAtom.GenericExtensions.to_xml_data_model entry.extensions ]
+
+
+    let of_xml_data_model entry tree =
+      match parse_xml_data_model tree with
+      | `Etag etag -> {entry with etag}
+      | `Id id -> {entry with id}
+      | `Updated updated -> {entry with updated}
+      | `Edited edited -> {entry with edited}
+      | `Category category -> {entry with category}
+      | `Published published -> {entry with published}
+      | `Title title -> {entry with title}
+      | `Link link -> {entry with links= link :: entry.links}
+      | `Author author -> {entry with author}
+      | `Summary summary -> {entry with summary}
+      | `Namespace -> entry
+      | `Other
+          GapiCore.AnnotatedTree.Node
+            ( [`Element; (`Name "pageName"); (`Namespace ns)]
+            , [(GapiCore.AnnotatedTree.Leaf ([`Text], v))] )
+        when ns = ns_sites ->
+          {entry with pageName= v}
+      | `Other
+          GapiCore.AnnotatedTree.Node
+            ( [`Element; (`Name "revision"); (`Namespace ns)]
+            , [(GapiCore.AnnotatedTree.Leaf ([`Text], v))] )
+        when ns = ns_sites ->
+          {entry with revision= int_of_string v}
+      | `Other
+          GapiCore.AnnotatedTree.Node
+            ( [`Element; (`Name "content"); (`Namespace ns)]
+            , [ (GapiCore.AnnotatedTree.Leaf
+                  ([`Attribute; (`Name "type"); (`Namespace "")], "xhtml"))
+              ; (GapiCore.AnnotatedTree.Node _ as cs) ] )
+        when ns = GdataAtom.ns_atom ->
+          let html = GdataUtils.data_to_xml_string cs in
+          {entry with content= Xhtml html}
+      | `Other
+          GapiCore.AnnotatedTree.Node
+            ( [`Element; (`Name "content"); (`Namespace ns)]
+            , [ (GapiCore.AnnotatedTree.Leaf
+                  ([`Attribute; (`Name "type"); (`Namespace "")], content_type))
+              ; (GapiCore.AnnotatedTree.Leaf
+                  ([`Attribute; (`Name "src"); (`Namespace "")], src)) ] )
+        when ns = GdataAtom.ns_atom ->
+          {entry with content= Attachment (content_type, src)}
+      | _ ->
+          let extensions =
+            GdataAtom.GenericExtensions.of_xml_data_model entry.extensions tree
+          in
+          {entry with extensions}
+
+  end
+
+  module Feed =
+    GdataAtom.MakeFeed (Entry) (GdataAtom.Link) (GdataAtom.GenericExtensions)
+
+  let parse_entry =
+    GdataAtom.data_model_to_entry Entry.of_xml_data_model Entry.empty
+
+
+  let parse_feed = Feed.parse_feed
+
+  let entry_to_data_model =
+    GdataAtom.element_to_data_model get_sites_prefix Entry.to_xml_data_model
+
+
+  let feed_to_data_model =
+    GdataAtom.element_to_data_model get_sites_prefix Feed.to_xml_data_model
+
+
+  let make_category name =
+    let category_scheme = "http://schemas.google.com/g/2005#kind" in
+    { GdataAtom.Category.empty with
+      GdataAtom.Category.scheme= category_scheme
+    ; term= "http://schemas.google.com/sites/2008#" ^ name
+    ; label= name }
+
+
+  let attachment_category = make_category "attachment"
+
+  let webpage_category = make_category "webpage"
+
+  module Rel = struct
+    type t = [`Self | `Alternate | `Edit | `Media | `Parent | `Template]
+
+    let parent = "http://schemas.google.com/sites/2008#parent"
+
+    let template = "http://schemas.google.com/sites/2008#template"
+
+    let to_string (t: t) =
+      match t with
+      | `Template -> template
+      | `Parent -> parent
+      | `Self as r -> GdataAtom.Rel.to_string r
+      | `Alternate as r -> GdataAtom.Rel.to_string r
+      | `Edit as r -> GdataAtom.Rel.to_string r
+      | `Media as r -> GdataAtom.Rel.to_string r
+
+  end
+
+  let get_link rel c = GdataAtom.find_url_generic (module Rel) rel
+
+  let make_link rel entry =
+    let link =
+      { GdataAtom.Link.empty with
+        GdataAtom.Link.rel= Rel.to_string rel; href= entry.Entry.id }
+    in
+    match rel with
+    | `Template | `Parent ->
+        {link with GdataAtom.Link._type= "application/atom+xml"}
+    | _ -> link
 
 end
--- lib/GdataSitesService.ml
+++ lib/GdataSitesService.ml
@@ -12,19 +12,16 @@ let parse_site_feed = GdataUtils.parse_xml_response Site.parse_feed
 
 let parse_site_entry = GdataUtils.parse_xml_response Site.parse_entry
 
-let get_url ?(rel = `Edit) links =
-  GdataAtom.find_url rel links
+let get_url ?(rel= `Edit) links = GdataAtom.find_url rel links
 
-let get_media_url ?(rel = `EditMedia) links =
-  GdataAtom.find_url rel links
+let get_media_url ?(rel= `EditMedia) links = GdataAtom.find_url rel links
 
-let get_etag etag =
-  GdataUtils.string_to_option etag
+let get_etag etag = GdataUtils.string_to_option etag
 
 let get_url_etag_content ?rel entry =
   let url = get_url entry.Content.Entry.links ?rel in
   let etag = get_etag entry.Content.Entry.etag in
-    (url, etag)
+  (url, etag)
 
 
 let get_content base_url resource_id session =
@@ -32,12 +29,73 @@ let get_content base_url resource_id session =
   GdataService.query ~version url parse_content_entry session
 
 
-let query_content_list ~url ?etag ?parameters session =
-  GdataService.query ~version ?etag url parse_content_feed session
+type basic_parameters =
+  [ `Max_results of int
+  | `Published_max of Netdate.t
+  | `Published_min of Netdate.t
+  | `Q of string
+  | `Start_index of int ]
+
+let query_to_key_value = function
+  | `Max_results n -> ("max-results", string_of_int n)
+  | `Published_max date -> ("published-max", GapiDate.to_string date)
+  | `Published_min date -> ("published-min", GapiDate.to_string date)
+  | `Start_index n -> ("published-min", string_of_int n)
+  | `Q s -> ("q", s)
 
 
-let query_site ~url ?etag ?parameters session =
-  GdataService.query ~version ?etag url parse_site_feed session
+let parameters_of_query f query = Option.map (List.map f) query
+
+let query_site ?(domain= "site") ?site ?etag ?query session =
+  let url = endpoint ^ "site/" ^ domain ^ "/" in
+  let url = match site with Some site -> url ^ site ^ "/" | None -> url in
+  let query_parameters =
+    parameters_of_query
+      (function
+          | `Include_all_sites b -> ("include-all-sites", string_of_bool b)
+          | `With_mappings b -> ("with-mappings", string_of_bool b)
+          | #basic_parameters as q -> query_to_key_value q)
+      query
+  in
+  GdataService.query ~version ?etag ?query_parameters url parse_site_feed
+    session
+
+
+let string_of_category = function
+  | `Announcement -> "announcement"
+  | `AnnouncementPage -> "announcementpage"
+  | `Attachment -> "attachment"
+  | `Comment -> "comment"
+  | `FileCabinet -> "filecabinet"
+  | `ListItem -> "listitem"
+  | `ListPage -> "listpage"
+  | `WebPage -> "webpage"
+  | `WebAttachment -> "webattachment"
+  | `Template -> "template"
+
+
+let content_url ?(domain= "site") site =
+  endpoint ^ "content/" ^ domain ^ "/" ^ site ^ "/"
+
+
+let query_content_list ?domain ?etag ?query site session =
+  let url = content_url ?domain site in
+  let query_parameters =
+    parameters_of_query
+      (function
+          | `Updated_max date -> ("published-max", GapiDate.to_string date)
+          | `Updated_min date -> ("published-min", GapiDate.to_string date)
+          | `Ancestor s -> ("ancestor", s)
+          | `Include_deleted b -> ("include-deleted", string_of_bool b)
+          | `Include_draft b -> ("include-draft", string_of_bool b)
+          | `Category c -> ("category", string_of_category c)
+          | `Parent s -> ("parent", s)
+          | `Path p -> ("path", "/" ^ String.concat "/" p)
+          | #basic_parameters as q -> query_to_key_value q)
+      query
+  in
+  GdataService.query ~version ?query_parameters ?etag url parse_content_feed
+    session
 
 
 let media_source_payload media =
@@ -45,17 +103,19 @@ let media_source_payload media =
   | GapiMediaResource.String s -> s
   | _ -> failwith "media_source_payload not supported"
 
+
 let make_payload ?media_source data_to_tree data =
   match media_source with
   | None ->
-     let tree = Content.entry_to_data_model data in
-     let xml = GdataUtils.data_to_xml_string tree in
+      let tree = Content.entry_to_data_model data in
+      let xml = GdataUtils.data_to_xml_string tree in
       GapiCore.PostData.Body
         (GapiCore.PostData.String xml, GdataCore.default_content_type)
   | Some media_source ->
-     let payload = media_source_payload media_source in
+      let payload = media_source_payload media_source in
       GapiCore.PostData.Body
-        (GapiCore.PostData.String payload, media_source.GapiMediaResource.content_type)
+        ( GapiCore.PostData.String payload
+        , media_source.GapiMediaResource.content_type )
 
 
 let make_multipart ?media_source data_to_tree data =
@@ -92,25 +152,97 @@ let make_multipart ?media_source data_to_tree data =
       GapiCore.PostData.Body (GapiCore.PostData.String body, content_type)
 
 
-let create_content ~url ?media_source entry session =
+let create_content ?domain ?media_source site entry session =
+  let url = content_url ?domain site in
   GapiService.service_request_with_data GapiRequest.Create
     (make_multipart ?media_source Content.entry_to_data_model)
     ~version entry url
     (fun pipe _ -> parse_content_entry pipe)
     session
 
+
+let create_attachment ?domain ?summary ~parent ~title ~content_type ~payload
+    ~site session =
+  let media_source =
+    { GapiMediaResource.source= GapiMediaResource.String payload
+    ; name= title
+    ; content_type
+    ; content_length= String.length payload |> Int64.of_int }
+  in
+  let entry =
+    { Content.Entry.empty with
+      Content.Entry.category= Content.attachment_category
+    ; title= {GdataAtom.Title.empty with GdataAtom.Title.value= title}
+    ; links= [Content.make_link `Parent parent] }
+  in
+  let entry =
+    match summary with
+    | None -> entry
+    | Some summary ->
+        { entry with
+          Content.Entry.summary=
+            {entry.Content.Entry.summary with GdataAtom.Summary.value= summary}
+        }
+  in
+  create_content ?domain ~media_source site entry session
+
+
+let create_webpage ?domain ?summary ?parent ~title ?pageName ?html ?template
+    ~site session =
+  let links = [] in
+  let links =
+    match parent with
+    | Some parent -> Content.make_link `Parent parent :: links
+    | None -> links
+  in
+  let links =
+    match template with
+    | Some template -> Content.make_link `Template template :: links
+    | None -> links
+  in
+  let entry =
+    { Content.Entry.empty with
+      Content.Entry.category= Content.webpage_category
+    ; title= {GdataAtom.Title.empty with GdataAtom.Title.value= title}
+    ; links }
+  in
+  let entry =
+    match pageName with
+    | None -> entry
+    | Some pageName -> {entry with Content.Entry.pageName}
+  in
+  let entry =
+    match html with
+    | None -> entry
+    | Some html -> {entry with Content.Entry.content= Content.Entry.Xhtml html}
+  in
+  let entry =
+    match summary with
+    | None -> entry
+    | Some summary ->
+        { entry with
+          Content.Entry.summary=
+            {entry.Content.Entry.summary with GdataAtom.Summary.value= summary}
+        }
+  in
+  create_content ?domain site entry session
+
+
 let delete_content entry session =
-  let (url,etag) = get_url_etag_content entry in
+  let url, etag = get_url_etag_content entry in
   GdataService.delete ~version ?etag url session
 
+
 let update_content ?media_source entry session =
-  let (url,etag) = get_url_etag_content entry in
-  let (make,url) = match media_source with
-      Some _ -> make_payload,get_media_url entry.Content.Entry.links
-    | None -> make_multipart,url in
-  "Update with " ^ url |> print_endline;
+  let url, etag = get_url_etag_content entry in
+  let url =
+    match media_source with
+    | Some _ -> get_media_url entry.Content.Entry.links
+    | None -> url
+  in
   GapiService.service_request_with_data GapiRequest.Update
-    (make ?media_source Content.entry_to_data_model)
+    (make_multipart ?media_source Content.entry_to_data_model)
     ~version ?etag entry url
     (fun pipe _ -> parse_content_entry pipe)
     session
+
